246f9424e3f458acca88d52cbd0dfd09c996804e
Shiul93 <lfllamas93@gmail.com>
2016-12-16 10:27:51 +0100

    Updated develop

--- robobo-scratch-extension-develop.js
+++ robobo-scratch-extension-develop.js
@@ -139,8 +139,8 @@
       }else if (mtype=='centimeters'){
         console.log('moveRobobo by '+mtype);
         //TODO Medir ruedas y hacer una conversión grados/centimetros
-        console.log('Quantity:'+(parseInt(quantity)+1.1)/0.5503);
-        rem.moveWheelsByDegree(round((parseInt(quantity)+1.1)/0.5503));
+        console.log('Quantity:'+Math.round(parseInt(quantity)+1.1)/0.5503);
+        rem.moveWheelsByDegree(Math.round((parseInt(quantity)+1.1)/0.5503));
       }
 
     };
