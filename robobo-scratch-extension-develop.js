4b73f996c734ffd6366bc443d64fdb4e04649359
Shiul93 <lfllamas93@gmail.com>
2017-02-06 09:58:09 +0100

    updated develop

--- robobo-scratch-extension-develop.js
+++ robobo-scratch-extension-develop.js
@@ -336,7 +336,7 @@
     };
 
     //Hat function that checks for new faces
-    ext.newFace = function() {
+    ext.newFaceTest = function() {
       if (newface){
         newface = false;
         return true;
@@ -553,7 +553,7 @@
           ['r', 'read brightness','readBrightnessLevel'],//v
           ['r', 'read color at %m.colorchan channel','readMeasuredColor'],//v
 
-          ['h', 'when face is detected','newFace'],//v
+          ['h', 'when face is detected','newFaceTest'],//v
           ['h', 'when face is lost','lostFace'],//v
 
           ['h', 'On error','onError'],//v
