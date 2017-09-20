8dc0096e1c734f015f797caf90f4fa5becf58588
Gervasio Varela Fernández <gervasio.varela@mytechia.com>
2017-09-20 18:29:28 +0200

    Added new stable versión for Robobo 2.0

---
+++ 2/utilities.js
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright 2016 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ * Copyright 2016 Luis Llamas <luis.llamas@mytechia.com>
+ * Copyright 2016 Gervasio Varela <gervasio.varela@mytechia.com>
+ * <p>
+ * This file is part of Robobo Scratch Extension.
+ * <p>
+ * Robobo Scratch Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Robobo Scratch Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Robobo Scratch Extension.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+
+ /** Checks whether a value is inside an specified range */
+function rangeFun(input,type,r1,r2) {		
+    	
+    if (type == "between"){		
+      if(r1<r2){		
+        if ((input>r1)&&(input<r2)){		
+          return true;		
+        }else {		
+          return false;		
+        }		
+      }else {		
+        if ((input>r2)&&(input<r1)){		
+          return true;		
+        }else {		
+          return false;		
+        }		
+      }		
+    }else {		
+      if(r1<r2){		
+        if ((input<r1)||(input>r2)){		
+          return true;		
+        }else {		
+          return false;		
+        }		
+      }else {		
+        if ((input<r2)||(input>r1)){		
+          return true;		
+        }else {		
+          return false;		
+        }		
+      }		
+    }		
+  };
+
+
+/** Tranforms TAP coords to "face zones" */
+function coordsToZone(x, y){
+    
+    if (y<17){
+        return "forehead";
+    }else if (rangeFun(y,"between",17,56) && rangeFun(x,"between", 15, 85)){
+        return "eye";
+    }else if (rangeFun(y,"between",65,77) && rangeFun(x,"between", 25, 75)){
+        return "mouth";
+    }else if (rangeFun(x,"between",0,15)){
+        return "left";
+    }else if (rangeFun(x,"between",85,100)){
+        return "right";
+    }else if (rangeFun(y,"between",77,100) && rangeFun(x,"between", 15, 85)){
+        return "chin";
+    }
+}
